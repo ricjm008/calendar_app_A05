8ef60ee269df22715ba678b16d377f7d61734282
Jared Rice <ricjm008@mymail.unisa.edu.au>
2021-04-06 13:24:57 +0930

    updated formatting

--- script.js
+++ script.js
@@ -9,20 +9,36 @@ const timeBlocks = [
     "2 PM",
     "3 PM",
     "4 PM"
-]
+];
+const events = {
+
+}
+const today = dayjs();
+$currentDay.text(`Today is ${today.format("dddd, D MMMM h:mm A")}`)
 for (var i = 0; i < timeBlocks.length; i++) {
     // Create a new `<div>` for each ability and its text content
     const $timeBlock = $(`
-    <li class="time-block">
-    ${timeBlocks[i]}
-        <form>
-            <label for="timeBlock${i}">
-                Event: 
-            </label>
-            <input type="text" name="timeBlock${i}"/>
-            <button class="saveBtn" id="submit"/>
+    <div class="time-block row">
+    <h4 class="hour col">${timeBlocks[i]}</h4>
+        <form class="col-6 description">
+            <label for="timeBlock${i}">Event: </label>
+            <input type="text" name="timeBlock${i}" id="event"/>
         </form>
-    </li>
+        <button class="saveBtn col" id="submit"><i>Save</i></button>
+    </div>
     `);
     $dayPlanner.append($timeBlock);
-  };
+    if (today.format("H") > i + 9) {
+        $timeBlock.addClass("past")
+    } else if (today.format("H") == i + 9) {
+        $timeBlock.addClass("present")
+    } else {
+        $timeBlock.addClass("future")
+    }
+};
+$(".saveBtn").on("click", function () {
+    let event = $("#event").val;
+
+    localStorage.setItem("events", );
+})
+

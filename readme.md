cc9c8c1310e2bf88976d94bc234f63a45d718e2b
Jared Rice <ricjm008@mymail.unisa.edu.au>
2021-04-06 18:06:13 +0930

    updated readme and script

---
+++ readme.md
@@ -0,0 +1,4 @@
+## Calendar App Assignment 05
+This assignment uses third=party apis linked into index.html using urls. Below is a screenshot of the deployed application, and a link to the live deployment:
+![]()
+[]()
--- script.js
+++ script.js
@@ -10,35 +10,55 @@ const timeBlocks = [
     "3 PM",
     "4 PM"
 ];
-const events = {
-
-}
+const $event1 = $("#event0");
+const $event2 = $("#event1");
+const $event3 = $("#event2");
+const $event4 = $("#event3");
+const $event5 = $("#event4");
+const $event6 = $("#event5");
+const $event7 = $("#event6");
+const $event8 = $("#event7");
+let events = {
+    0: $event1.value.trim(),
+    1: $event2.value.trim(),
+    2: $event3.value.trim(),
+    3: $event4.value.trim(),
+    4: $event5.value.trim(),
+    5: $event6.value.trim(),
+    6: $event7.value.trim(),
+    7: $event8.value.trim()
+};
 const today = dayjs();
 $currentDay.text(`Today is ${today.format("dddd, D MMMM h:mm A")}`)
 for (var i = 0; i < timeBlocks.length; i++) {
     // Create a new `<div>` for each ability and its text content
     const $timeBlock = $(`
     <div class="time-block row">
-    <h4 class="hour col">${timeBlocks[i]}</h4>
+        <h4 class="hour col">${timeBlocks[i]}</h4>
         <form class="col-6 description">
-            <label for="timeBlock${i}">Event: </label>
-            <input type="text" name="timeBlock${i}" id="event"/>
+            <label class="description" for="timeBlock${i}">Event: </label>
+            <input type="text" class="description" id="event${i}" name="timeBlock${i}"/>
         </form>
+        <div id="eventsLogged${i}"></div>
         <button class="saveBtn col" id="submit"><i>Save</i></button>
     </div>
     `);
+    events = JSON.parse(localStorage.getItem("events"));
+    const $eventsLogged = $(`#eventsLogged${i}`);
+    if (events == !null) {
+        $eventsLogged = events;
+    }
     $dayPlanner.append($timeBlock);
     if (today.format("H") > i + 9) {
         $timeBlock.addClass("past")
     } else if (today.format("H") == i + 9) {
         $timeBlock.addClass("present")
-    } else {
+    } else if (today.format("H") < i + 9) {
         $timeBlock.addClass("future")
     }
+    
 };
 $(".saveBtn").on("click", function () {
-    let event = $("#event").val;
-
-    localStorage.setItem("events", );
-})
+        localStorage.setItem("events", JSON.stringify(events));
+});
 
